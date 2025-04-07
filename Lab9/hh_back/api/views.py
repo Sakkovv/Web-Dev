133271a91a4980344c89dd183e8bb3574dcb8baf
Sakkovv <zhumabek.2006@gmail.com>
2025-04-07 11:11:17 +0500

    upd and improve

--- Lab9/hh_back/api/views.py
+++ Lab9/hh_back/api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseNotFound
+from .models import Company, Vacancy
 
-# Create your views here.
+def company_list(request):
+    companies = Company.objects.all().values('id', 'name', 'description', 'city', 'address')
+    return JsonResponse(list(companies), safe=False)
+
+def company_detail(request, id):
+    try:
+        company = Company.objects.values('id', 'name', 'description', 'city', 'address').get(id=id)
+        return JsonResponse(company)
+    except Company.DoesNotExist:
+        return HttpResponseNotFound("Компания не найдена")
+
+def company_vacancies(request, id):
+    vacancies = Vacancy.objects.filter(company_id=id).values('id', 'name', 'description', 'salary')
+    return JsonResponse(list(vacancies), safe=False)
+
+def vacancy_list(request):
+    vacancies = Vacancy.objects.all().values('id', 'name', 'description', 'salary', 'company_id')
+    return JsonResponse(list(vacancies), safe=False)
+
+def vacancy_detail(request, id):
+    try:
+        vacancy = Vacancy.objects.values('id', 'name', 'description', 'salary', 'company_id').get(id=id)
+        return JsonResponse(vacancy)
+    except Vacancy.DoesNotExist:
+        return HttpResponseNotFound("Вакансия не найдена")
+
+def top_ten_vacancies(request):
+    vacancies = Vacancy.objects.all().order_by('-salary').values('id', 'name', 'description', 'salary', 'company_id')[:10]
+    return JsonResponse(list(vacancies), safe=False)
